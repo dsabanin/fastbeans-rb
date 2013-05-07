4c781a9e0eefe26c60c3621279c01b7d536987f3
Dima Sabanin <sdmitry@gmail.com>
2013-05-07 11:03:31 -0400

    Simple debugging and benchmarking code

--- lib/fastbeans.rb
+++ lib/fastbeans.rb
@@ -3,4 +3,26 @@ require "fastbeans/errors"
 require "fastbeans/client"
 
 module Fastbeans
+  class << self
+    # Debug mode
+    attr_accessor :debug_mode
+
+    def debug(str_or_exc)
+      case str_or_exc
+        when String 
+          STDERR.puts("[#{Time.now}] #{str}") if self.debug_mode
+        when Exception
+          debug("Exception: #{str_or_exc.message}\n#{str_or_exc.backtrace.join("\n")}")
+      end
+    end
+
+    def benchmark(str, &blk)
+      debug(str)
+      t1 = Time.now
+      blk.call
+    ensure
+      t2 = Time.now
+      debug("Time spent: #{t2-t1}s")
+    end
+  end
 end
--- lib/fastbeans/client.rb
+++ lib/fastbeans/client.rb
@@ -6,10 +6,11 @@ module Fastbeans
 
   class Client
     CALL_CACHE_SIZE=100
-
+    MAX_RETRIES=3
+    
     attr_reader :call_cache
 
-    def initialize(host, port, cache_size=nil)
+    def initialize(host="127.0.0.1", port=12345, cache_size=nil)
       @host, @port = host, port
       @cache_size ||= CALL_CACHE_SIZE
       @call_cache = Rufus::Lru::SynchronizedHash.new(@cache_size)
@@ -25,27 +26,33 @@ module Fastbeans
       connect!(@host, @port)
     end
 
+
     def connect!(host, port)
+      Fastbeans.debug("Connecting to #{host}:#{port}")
       @sock = TCPSocket.new(host, port)
       @sock.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
       @mutex = Mutex.new
     end
 
     def call(*data)
-      retries = 0
-      begin
-        call_without_retries(*data)
-      rescue Fastbeans::RemoteConnectionFailed => e
-        if retries < 3
-          retries += 1
-          begin
-            reconnect!
-          rescue => e
-            raise RemoteConnectionDead, e.message
+      Fastbeans.benchmark("Calling: #{data.inspect}") do
+        retries = 0
+        begin
+          call_without_retries(*data)
+        rescue Fastbeans::RemoteConnectionFailed => e
+          debug(e)
+          if retries < MAX_RETRIES
+            debug("Retrying (#{retries} out of #{MAX_RETRIES} retries)")
+            retries += 1
+            begin
+              reconnect!
+            rescue => e
+              raise RemoteConnectionDead, e.message
+            end
+            retry
+          else
+            raise RemoteConnectionDead, "#{e.message} (#{retries} retries)"
           end
-          retry
-        else
-          raise RemoteConnectionDead, "#{e.message} (#{retries} retries)"
         end
       end
     end
