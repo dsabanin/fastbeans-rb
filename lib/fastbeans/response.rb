f812a2f8945d0078a9844350e7316c592eb5e8b0
Dima Sabanin <sdmitry@gmail.com>
2013-06-13 10:31:48 -0400

    treat whitespace as _

--- lib/fastbeans/response.rb
+++ lib/fastbeans/response.rb
@@ -54,6 +54,7 @@ module Fastbeans
       word.gsub!(/([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
       word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
       word.tr!("-", "_")
+      word.gsub!(/\s+/, "_")
       word.downcase!
       word
     end
