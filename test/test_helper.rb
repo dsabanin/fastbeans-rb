6dc31de2e75091296ed8349233a5d1fe7f670101
Dima Sabanin <sdmitry@gmail.com>
2013-06-14 10:04:23 -0400

    Test suite support

---
+++ test/test_helper.rb
@@ -0,0 +1,20 @@
+require 'minitest/unit'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/setup'
+
+require 'fastbeans'
+
+class MockConnection
+  def initialize(*any); end
+
+  def call(*data)
+    data
+  end
+
+  attr_accessor :socket
+
+  def with_socket
+    yield(socket)
+  end
+end
