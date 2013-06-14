03d541692496cc5c191979728ec9184811da66d8
Dima Sabanin <sdmitry@gmail.com>
2013-06-14 10:04:43 -0400

    Helper tests

---
+++ test/fastbeans_test.rb
@@ -0,0 +1,9 @@
+require_relative 'test_helper'
+
+class FastbeansTest < MiniTest::Unit::TestCase
+
+  def test_should_autocreate_exceptions
+    assert_equal Fastbeans::RemoteException, Fastbeans.exception('RemoteException')
+    assert_equal 'Fastbeans::SomeException', Fastbeans.exception('SomeException').to_s
+  end
+end
